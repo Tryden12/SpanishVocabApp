3430a611d3d63263cb45d8e57aa2076ff6a4b3cc
Tyler Ryden <tryden11@gmail.com>
2022-04-03 12:54:36 -0500

    App added to Google Play Store

---
+++ app/src/main/java/com/tryden/spanishvocab/Word.kt
@@ -0,0 +1,14 @@
+package com.tryden.spanishvocab
+
+import java.io.Serializable
+
+class Word(
+    var spanishWord    : String = "",
+    var englishWord    : String = ""
+) : Serializable {
+
+    override fun toString(): String {
+        return "Words(spanishword=${spanishWord}, " +
+                "englishword=${englishWord}"
+    }
+}
